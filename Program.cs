6c7ba4c522e86d11509bdfa4d54f19af4a35ddc3
devinvisible <devinvisible@gmail.com>
2017-03-02 12:38:45 -0500

    Added Discord.Net package and simple echo test code

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
-﻿namespace DiscordBot
+﻿using Discord;
+using System.Threading.Tasks;
+
+namespace DiscordBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program().Run().GetAwaiter().GetResult();
+
+        public async Task Run()
         {
+            var client = new DiscordClient();
+
+            client.MessageReceived += async (s, e) =>
+            {
+                if (!e.Message.IsAuthor)
+                    await e.Channel.SendMessage(e.Message.Text);
+            };
+
+            client.ExecuteAndWait(async () => {
+                await client.Connect("aaaaabbbbbbcccccdddddeeeeefffffggggg", TokenType.Bot);
+            });
+
+            // Block this task until the program is exited.
+            await Task.Delay(-1);
         }
     }
 }
